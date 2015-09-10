12d24a5d6bd80d723d5c1565e99eb685f6cbec94
JDarkGreen <jgomez.4net@gmail.com>
2015-09-10 11:28:56 -0500

    modificar breadcrum no mostrar categoria

--- wp-content/themes/maletek/woocommerce/global/breadcrumb.php
+++ wp-content/themes/maletek/woocommerce/global/breadcrumb.php
@@ -16,21 +16,29 @@ if ( $breadcrumb ) {
 
 	echo $wrap_before;
 
+
 	foreach ( $breadcrumb as $key => $crumb ) {
 
 		echo $before;
 
-		if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) {
-			echo '<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>';
-		} else {
-			echo '<span class="last-breadcrumb">' . esc_html( $crumb[0] ) . '<span>';
-		}
+		// esta parte está modificada para propósitoss de visualización donde * es la linea de modificacion
+		// si se encuentra con una categoría "producto" no la mostraremos 
 
-		echo $after;
+		if ( $crumb[0] !== "productos" ) {   //*
 
-		if ( sizeof( $breadcrumb ) !== $key + 1 ) {
-			echo $delimiter;
-		}
+			if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) {
+				echo '<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>';
+			} else {
+				echo '<span class="last-breadcrumb">' . esc_html( $crumb[0] ) . '<span>';
+			}
+
+			echo $after;
+
+			if ( sizeof( $breadcrumb ) !== $key + 1 ) {
+				echo $delimiter;
+			}
+
+		} //*
 
 	}
 
